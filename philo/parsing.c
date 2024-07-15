a184cd610b456cdc737f097f68e943c97848bd73
Hamza Bettal <hbettal@e3r11p10.1337.ma>
2024-07-15 22:23:55 +0100

    الفلاسفة يموتون

---
+++ philo/parsing.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbettal <hbettal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 21:50:04 by hbettal           #+#    #+#             */
+/*   Updated: 2024/07/14 00:21:09 by hbettal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0') && i < n - 1)
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+static long	no_overflow(int sign, unsigned long long n, int count)
+{
+	if ((n > LLONG_MAX || count > 19) && sign == -1)
+		(write(2, "Error\n", 6), exit(1));
+	if ((n > LLONG_MAX || count > 19) && sign == 1)
+		(write(2, "Error\n", 6), exit(1));
+	return (n * sign);
+}
+
+long	ft_atoi(char *str)
+{
+	int		i;
+	int		count;
+	int		sign;
+	long	n;
+
+	i = 0;
+	count = 0;
+	sign = 1;
+	n = 0;
+	if (str[i] == '-' || str[i] == '+')
+		if (str[i++] == '-')
+			sign *= -1;
+	if (str[i] == '\0' || str[i] < '0' || str[i] > '9')
+		(write(2, "Error\n", 6), exit(1));
+	while (str[i] == '0')
+		i++;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			(write(2, "Error\n", 6), exit(1));
+		n = (n * 10) + (str[i++] - '0');
+		count++;
+	}
+	return (no_overflow(sign, n, count));
+}
+
+int	parse(char **av, int ac)
+{
+	if (ac > 4 && ac < 7)
+	{
+		if (ft_atoi(av[1]) <= 0 || ft_atoi(av[1]) >= 200)
+			return (write(2, "wrong input\n", 12), 1);
+			if (ft_atoi(av[2]) < 61 || ft_atoi(av[3]) < 61 || ft_atoi(av[4]) < 61)
+				return (write(2, "wrong input\n", 12), 1);
+		if (av[5] && ft_atoi(av[5]) < 1)
+			return (write(2, "wrong input\n", 12), 1);
+	}
+	else
+		return (write(2, "wrong input\n", 12), 1);
+	return (0);
+}
---
+++ philo/philo_born.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_born.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbettal <hbettal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 12:38:21 by hbettal           #+#    #+#             */
+/*   Updated: 2024/07/15 14:06:03 by hbettal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+size_t	get_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
+int		fill_table(t_table *table, char **av, int ac)
+{
+	(void)ac;
+	table->num_of_philo = ft_atoi(av[1]);
+	table->time_to_die = ft_atoi(av[2]);
+	table->time_to_eat = ft_atoi(av[3]);
+	table->time_to_sleep = ft_atoi(av[4]);
+	if (pthread_mutex_init(&table->print, NULL) 
+	|| pthread_mutex_init(&table->dead_mutex, NULL) 
+	|| pthread_mutex_init(&table->meal_mutex, NULL))
+		return (write(2, "Error\n", 6), 1);
+	table->dead = 0;
+	return (0);
+}
+
+int	philo_birth(char **av, int ac)
+{
+	t_philo	*philo;
+	t_table	*table;
+	int		i;
+	
+	i = -1;
+	table = malloc(sizeof(t_table) * ft_atoi(av[1]));
+	if (fill_table(table, av, ac))
+		return (1);
+	philo = malloc(sizeof(t_philo) * table->num_of_philo);
+	table->forks = malloc(sizeof(pthread_mutex_t) * table->num_of_philo);
+	if (!philo || !table->forks)
+		(write(2, "Error\n", 6));
+	philo->table = table;
+	while (++i < philo->table->num_of_philo)
+	{
+		if (ac == 6)
+			philo->table[i].num_of_meals = ft_atoi(av[5]);
+		else
+			philo->table[i].num_of_meals = -1;
+		1 && (philo[i].id = i + 1, philo[i].start = get_time());
+		1 && (philo[i].last_meal = philo[i].start, philo[i].table = table);
+		philo[i].r_fork = &table->forks[i];
+		philo[i].l_fork = &table->forks[(i + 1) % table->num_of_philo];
+		if (pthread_mutex_init(&table->forks[i], NULL))
+			return (write(2, "Error\n", 6), 1);
+	}
+	start_simulation(philo, table);
+	return (0);
+}
